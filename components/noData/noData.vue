fc4eb76b4efe2710d109a1ebd86d54718f49d300
li_haojie <m17554237921@163.com>
2020-02-06 16:18:46 +0800

    index

---
+++ components/noData/noData.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<image src="../../static/noData.png" mode="" class="img"></image>
+		暂无数据
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.content {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: #333;
+		font-size: 32upx;
+	}
+	
+	.img {
+		width: 160upx;
+		height: 120upx;
+	}
+</style>
